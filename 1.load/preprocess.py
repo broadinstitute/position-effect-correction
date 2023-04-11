0fc6ed69f98a1db1a5e96d7b9e4a8f5faeb928dd
alxndrkalinin <1107762+alxndrkalinin@users.noreply.github.com>
2023-04-11 16:00:54 -0400

    preprocess: remove rows with nans after feature select

--- 1.load/preprocess.py
+++ 1.load/preprocess.py
@@ -9,7 +9,7 @@ config = {
     "FEAT_SELECT_OPS": ["variance_threshold", "correlation_threshold", "drop_na_columns", "blocklist"]
 }
 
-def preprocess_profiles(ann_dframe):
+def preprocess_profiles(ann_dframe, remove_nan_rows=True):
 
     ann_dframe = normalize(ann_dframe, method="mad_robustize",
                            mad_robustize_epsilon=config["MAD_EPSILON"],
@@ -17,5 +17,9 @@ def preprocess_profiles(ann_dframe):
     
     ann_dframe = feature_select(ann_dframe, operation=config["FEAT_SELECT_OPS"],
                                 image_features=config["INC_IMAGE_FEATURES"])
+    
+    # remove rows with any NaNs in features
+    if remove_nan_rows:
+        ann_dframe = ann_dframe[~ann_dframe.filter(regex="^(?!Metadata_)").isnull().T.any()]
 
     return ann_dframe
