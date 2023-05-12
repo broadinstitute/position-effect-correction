8f89a551b4b7d04c8999228da45454805b50d4aa
alxndrkalinin <1107762+alxndrkalinin@users.noreply.github.com>
2023-05-12 14:53:29 -0400

    rerun evalzoo after adding CC back as a feature; update plots

--- 3.correct/plot_utils.py
+++ 3.correct/plot_utils.py
@@ -1,34 +1,120 @@
+from pathlib import Path
+from typing import Any, Dict, Optional
+
 import numpy as np
+import pandas as pd
+
 import seaborn as sns
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
 
 
-def scatterplot(x, y, hue, **kwargs):
-    ax = plt.gca()
-    sns.scatterplot(x=x, y=y, hue=hue, ax=ax, **kwargs)
+def load_metrics(
+        metrics: dict,
+        metrics_columns: dict,
+        groupby_columns: dict,
+        results_dir: Path,
+        mertrics_filename: str,
+) -> pd.DataFrame:
+    """
+    Load metrics from parquet files and calculate mean values per groupby_columns.
 
-def kdeplot(x, y, hue, **kwargs):
-    ax = plt.gca()
-    sns.kdeplot(x=x, y=y, hue=hue ,ax=ax, **kwargs)
+    Parameters
+    ----------
+    metrics : dict
+        Dictionary of metrics to load.
+    metrics_columns : dict
+        Dictionary of metrics columns to rename.
+    groupby_columns : dict
+        Dictionary of columns to groupby.
+    results_dir : Path
+        Path to results directory.
+    mertrics_filename : str
+        Filename of metrics parquet file.
 
-def remove_inner_ticklabels(fig):
+    Returns
+    -------
+    metrics_df : pd.DataFrame
+        Dataframe of metrics.
+    """    
+    metrics_dfs = []
+
+    for subset, configs in metrics.items():
+        for config, config_dir in configs.items():
+            metric_cols = list(metrics_columns.keys())
+            metrics_path = results_dir / config_dir / f"{mertrics_filename}.parquet"
+            metrics_df = pd.read_parquet(metrics_path, columns=metric_cols + groupby_columns[config])
+            metrics_df = metrics_df.groupby(groupby_columns[config])[metric_cols].mean().reset_index()
+            # metrics_df.drop(groupby_columns[config], axis=1, inplace=True)
+            metrics_df.rename(columns=metrics_columns, inplace=True)
+            metrics_df["config"] = config
+            metrics_df["subset"] = subset
+            metrics_df["p<0.05"] = metrics_df["-log(pvalue)"] > 1.3
+            metrics_dfs.append(metrics_df)
+
+    metrics_df = pd.concat(metrics_dfs, axis=0)
+    return metrics_df
+
+
+def remove_inner_ticklabels(fig: plt.Figure) -> None:
+    """
+    Remove inner ticklabels from a figure.
+
+    Parameters
+    ----------
+    fig : matplotlib.figure.Figure
+        Figure to remove inner ticklabels from.
+
+    Returns
+    -------
+    None
+    """
     for ax in fig.axes:
         try:
             ax.label_outer()
         except:
             pass
 
+# redefine plot_map_per_config with type annotations
 def plot_map_per_config(
-        config_df,
-        config,
-        x_col="mAP",
-        y_col="-log(pvalue)",
-        hue_col="p<0.05",
-        style_col=None,
-        y_log=False,
-        ax_line=None,
-        figsave_path="output"):
+        config_df: pd.DataFrame,
+        config: str,
+        x_col: str = "mAP",
+        y_col: str = "-log(pvalue)",
+        hue_col: str = "p<0.05",
+        style_col: Optional[str] = None,
+        y_log: bool = False,
+        ax_line: Optional[Any] = None,
+        figsave_path: str = "output"
+) -> None:
+    """
+    Plot metrics for a given config.
+
+    Parameters
+    ----------
+    config_df : pd.DataFrame
+        Dataframe of metrics.
+    config : str
+        Config to plot.
+    x_col : str, optional
+        Column to use as x-axis, by default "mAP".
+    y_col : str, optional
+        Column to use as y-axis, by default "-log(pvalue)".
+    hue_col : str, optional
+        Column to use as hue, by default "p<0.05".
+    style_col : str, optional
+        Column to use as style, by default None.
+    y_log : bool, optional
+        Whether to use log scale for y-axis, by default False.
+    ax_line : Any, optional
+        Axis line to plot, by default None.
+    figsave_path : str, optional
+        Path to save figure, by default "output".
+
+    Returns
+    -------
+    None
+    """
     subsets = config_df['subset'].unique()
     n_subsets = len(subsets)
     
