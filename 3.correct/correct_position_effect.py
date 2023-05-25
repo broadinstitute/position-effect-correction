6c19d523f91c5dd9dd1c36b3d230fa1f56030722
alxndrkalinin <1107762+alxndrkalinin@users.noreply.github.com>
2023-05-25 15:58:37 -0400

    add parallelized cell count regression

--- 3.correct/correct_position_effect.py
+++ 3.correct/correct_position_effect.py
@@ -1,5 +1,7 @@
 from typing import Optional
 
+from concurrent import futures
+
 import pandas as pd
 
 from scipy.stats import median_abs_deviation
@@ -71,11 +73,55 @@ def regress_out_cell_counts(ann_df: pd.DataFrame, cc_col: str, min_unique: int =
 
     feature_cols = df.filter(regex="^(?!Metadata_)").columns.to_list()
     feature_cols.remove(cc_col)
+    feature_cols = [feature for feature in feature_cols if df[feature].nunique() > min_unique]
 
     for feature in tqdm(feature_cols):
-        if df[feature].nunique() > min_unique:
+        model = ols(f"{feature} ~ {cc_col}", data=df).fit()
+        df[f"{feature}"] = model.resid
+
+    if cc_rename is not None:
+        df.rename(columns={cc_col: cc_rename}, inplace=True)
+    return df
+
+
+# rewrite regress_out_cell_counts to parallelize it with concurrent.futures
+def regress_out_cell_counts_parallel(ann_df: pd.DataFrame, cc_col: str, min_unique: int = 100, cc_rename: Optional[str] = None, inplace: bool = True) -> pd.DataFrame:
+    """
+    Regress out cell counts from all features in a dataframe in parallel.
+
+    Parameters
+    ----------
+    ann_df : pandas.core.frame.DataFrame
+        DataFrame of annotated profiles.
+    cc_col : str
+        Name of column containing cell counts.
+    min_unique : int, optional
+        Minimum number of unique feature values to perform regression.
+    cc_rename : str, optional
+        Name to rename cell count column to.
+    inplace : bool, optional
+        Whether to perform operation in place.
+
+    Returns
+    -------
+    df : pandas.core.frame.DataFrame
+    """
+    df = ann_df if inplace else ann_df.copy()
+
+    feature_cols = df.filter(regex="^(?!Metadata_)").columns.to_list()
+    feature_cols.remove(cc_col)
+    feature_cols = [feature for feature in feature_cols if df[feature].nunique() > min_unique]
+
+    # rewrite main loop to parallelize it
+    def regress_out_cell_counts_parallel_helper(feature):
             model = ols(f"{feature} ~ {cc_col}", data=df).fit()
-            df[f"{feature}"] = model.resid
+            return {feature: model.resid}
+
+    with futures.ThreadPoolExecutor() as executor:
+        results = executor.map(regress_out_cell_counts_parallel_helper, feature_cols)
+
+    for res in results:
+        df.update(pd.DataFrame(res))
 
     if cc_rename is not None:
         df.rename(columns={cc_col: cc_rename}, inplace=True)
